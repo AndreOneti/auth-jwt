2de0fde7046700330d56e2647211b55058fbb6ce
Andre Luiz <andre@paconstrushop.com.br>
2020-07-09 17:19:32 -0300

    Test with docker
    Pre-setup before test

--- bin/server.js
+++ bin/server.js
@@ -2,9 +2,11 @@ const app = require('../src');
 const http = require('http');
 
 const port = process.env.PORT || 9001;
+const host = '0.0.0.0';
+
 app.set('port', port);
 
 const server = http.createServer(app);
 
-server.listen(port);
+server.listen(port, host);
 console.log('API rodando em http://localhost:' + port);
